2545b47a959a00e2d68daa8818690652393b9929
Xolmat <xolmatrw@gmail.com>
2021-01-16 13:19:11 +0300

    working on home page

---
+++ src/homepage.component.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import  "./homepage.styles.scss";
+ const HomePage = () => {
+   return(
+    <div className='homepage'>
+    <div className='directory-menu'>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>HATS</h1>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
+        </div>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>BOOKS</h1>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
+        </div>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>SHORTS</h1>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
+        </div>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>Jackets</h1>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
+        </div>
+    </div>
+</div>
+   );
+};
+
+export default HomePage;
